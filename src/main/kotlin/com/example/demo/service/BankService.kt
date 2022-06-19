c2e804b07d0654c91bfd1c6719de0d6e7eb4b513
TruongNM <truongnmt.dev@gmail.com>
2022-06-19 15:28:19 +0900

    Add mockk and use MockMvc to write controller test, add controller

---
+++ src/main/kotlin/com/example/demo/service/BankService.kt
@@ -0,0 +1,11 @@
+package com.example.demo.service
+
+import com.example.demo.datasource.BankDataSource
+import com.example.demo.model.Bank
+import org.springframework.stereotype.Service
+
+@Service
+class BankService(private val dataSource: BankDataSource) {
+
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+}
---
+++ src/test/kotlin/com/example/demo/controller/BankControllerTest.kt
@@ -0,0 +1,32 @@
+package com.example.demo.controller
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity.status
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class BankControllerTest {
+    
+    @Autowired
+    lateinit var mockMvc: MockMvc
+    
+    @Test
+    fun `shoud return all banks` () {
+        // when/then
+        mockMvc.get("/api/banks")
+            .andDo { print() }
+            .andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$[0].accountNumber") { value("1234") }
+                jsonPath("$[0].transactionFee") { value("17") }
+            }
+    }
+}
