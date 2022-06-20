75955e38c2bbb3ae1a68e0d77c31734dd5f46dbc
TruongNM <truongnmt.dev@gmail.com>
2022-06-20 19:26:56 +0900

    Use DirtiesContext to reintialize Spring boot application after a test

--- src/test/kotlin/com/example/demo/controller/BankControllerTest.kt
+++ src/test/kotlin/com/example/demo/controller/BankControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.*
 
 @SpringBootTest
@@ -174,6 +175,7 @@ internal class BankControllerTest @Autowired constructor (
     @TestInstance(Lifecycle.PER_CLASS)
     inner class DeleteExistingBank {
         @Test
+        @DirtiesContext
         fun `shoud delete the bank with the given account number` () {
             // given
             val accountNumber = 1234
